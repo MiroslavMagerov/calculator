5d6d4b6a44e002a73f68ce8df1811e26188f2453
Mirko <miroslav.magerov@gmail.com>
2024-02-13 23:18:47 +0100

    Empezado js, diseño acabado al completo.

---
+++ script.js
@@ -0,0 +1,12 @@
+function init() {
+    let displayZone = document.querySelector('#calculator-display-container');
+    let previousOp = displayZone.document.querySelector('h3');
+    let currentOp = displayZone.document.querySelector('h4');
+    let firstNum = 0, secondNum = 0;
+
+    
+
+    function addCurrent() {
+
+    }
+}
